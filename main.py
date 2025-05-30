38d1347b00761c31c8fb7cf39c9342209b12a80c
SedukhViacheslav <Sedukh@ya.ru>
2025-05-30 18:30:09 +0300

    add new func calc

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def area_circle(r):
-    s = 3.14 * r ** 2
-    return s
-
-def area_square_area(a):
-    S_s = a ** 2
-    return S_s
+def calc(a, b):
+    print(a + b)
+    print(a - b)
+    print(a * b)
+    print(a/b)
